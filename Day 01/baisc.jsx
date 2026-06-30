d4f97a6051859f9ca7e949839dc1af6f42469db3
Code-withAnsh <anshs9333@gmail.com>
2026-06-30 13:45:39 +0530

    first commit

---
+++ Day 01/baisc.jsx
@@ -0,0 +1,13 @@
+//this is the very basic information about react that react is a library created by meta
+//lets undderstand library and the framework , library is that where we got only boilerplate..
+//means if you want to create a website you need testing,routes,services many thing but library provide only
+///the boilerplate you have to manually install these but in framworks it is preinstalled you can directly use
+//but thats why it is slower means libraries have no prebuild unncessary code so it is fast .. 
+//so react is a library created by meta and angular is a framwork created by google
+// in other hand we know that we need these routes, services in out website so react intrdoucedd its own framwork called next js
+//it is a fullstack framework while angular is a frontend framwork
+
+//so now lets understand how react works.. react prevent the browser to reload...but how??
+// the browser so the website things by the DOM reacts do a thing reacts create a duplicate of that website virtual dom
+//let suppose user like a post only that like button color and counts increase so react analysis the old dom which is unliked
+//and new chages , now react reflect only the changes part on the main browser instead of reloading whole website
