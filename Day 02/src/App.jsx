7d8ebd7c73d0267f6717a83494e0486315641ee8
Code-withAnsh <anshs9333@gmail.com>
2026-06-30 18:05:42 +0530

    added day 2 of react learning

---
+++ Day 02/src/App.jsx
@@ -0,0 +1,12 @@
+//dekho sab karne ke baad jo steps hote hai npm create vite@latest --- then npm install -- then npm run dev
+//uske baad dher sare files ban jayenge phir aa gye app.jsx me isme phle se code tha delete kar diya
+//then type rafce and start yaad rkhna ye kaam tbhi karega wen you install  an extension ES7+ React/Redux/React-Native snippets by dsznajder
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+export default App //ab isko dekho humne export kiya aur main me jake dekho ye import kiya gya hai
+
