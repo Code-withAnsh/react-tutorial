f820f1c8e376779a9706228d40bf0433c99266a0
Code-withAnsh <anshs9333@gmail.com>
2026-07-09 15:19:57 +0530

    some modifications

--- Day 03/src/main.jsx
+++ Day 03/src/main.jsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById('root')).render(<App/>)
 //flow jo mai samjh paa rha index jo hai wo screen per chize ko
 //  dikhata hai aur uss se attach hai main aur main hi index tak 
 // chezo ko pahuchata hai example -- hum render kar rha hai app
-//  ko aur ye render hogaindex per jo display karega screen per 
+//  ko aur ye render hoga index per jo display karega screen per 
 // ab jo apna app hai whi render kar rha toh jitne bhi files
 //  banenge unko call kiya jayega app me hi aur import bhi mainly
 //  likha jata hai specific files me jaise app, eventlist3,footer wo call hota hai app me jisko main render karata hai main,jsx
